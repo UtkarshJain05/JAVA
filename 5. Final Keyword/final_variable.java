ca8140b5b65d73e6b1bf01a3c833818e2a8ce297
Utkarsh Jain <jain.utkarsh.5605@gmail.com>
2026-04-25 23:24:23 +0530

    Add files via upload

---
+++ 5. Final Keyword/final_variable.java
@@ -0,0 +1,10 @@
+
+public class final_variable {
+
+    public static void main(String args[]) {
+
+        final int A = 10;
+        System.out.println(A);
+        //  A++ or A = 20 will give error.
+    }
+}
